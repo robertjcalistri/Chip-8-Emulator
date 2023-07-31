69b2d89b94f09ef8888edfe43a34d29061e18a10
robertjcalistri <robertjcalistri@gmail.com>
2023-07-31 12:43:17 -0400

    Finish adding memory functionality and error checking

--- src/main.c
+++ src/main.c
@@ -1,9 +1,24 @@
 #include "memory.h"
 #include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+void initialize_memory();
 void load_rom(const char*);
+uint8_t read_memory(uint16_t);
+void write_memory(uint16_t, uint16_t);
 
 int main(int argc, char **argv) {
 
-    load_rom("C:\\Users\\harry\\OneDrive\\Desktop\\Chip-8-Emulator\\roms\\PONG");
+    if(argc < 2) {
+        printf("ERROR: expected 2 arguments\n");
+        printf("USE: [./exe][PATH/TO/ROM]");
+        exit(1);
+    }
+
+    char* rom_path = argv[1];
+    initialize_memory();
+    load_rom(rom_path);
+    
     return 0;
 }
--- src/memory.c
+++ src/memory.c
@@ -4,6 +4,7 @@
 #include <string.h>
 
 #define MEM_SIZE 4096
+#define MEM_START 0x200
 
 static uint8_t memory[MEM_SIZE];
 
@@ -19,20 +20,20 @@ void load_rom(const char* rom_path) {
     rom_file = fopen(rom_path, "r");
 
     if(rom_file == NULL){
-        perror("Failed to open file: ");
+        perror("ERROR: Failed to open file: ");
     }
     else{
-        printf("\nSuccessfully opened: %s\n", rom_path);
+        printf("\nSUCCESS: Successfully opened: %s\n", rom_path);
     }
 
     if(fseek(rom_file,0,SEEK_END) == -1) {
-        printf("Failed to fseek %s\n", rom_path);
+        printf("ERROR: Failed to fseek %s\n", rom_path);
         exit(EXIT_FAILURE);
     }
     int fsize = ftell(rom_file);
 
     if(fsize == -1) {
-        printf("Failed to ftell %s\n", rom_path);
+        printf("ERROR: Failed to ftell %s\n", rom_path);
         exit(EXIT_FAILURE);
     }
 
@@ -41,12 +42,45 @@ void load_rom(const char* rom_path) {
 
     char buffer[fsize];
     fread(buffer, 1, fsize, rom_file);
+
+    if(fsize <= MEM_SIZE) {
+        for(int i = 0; i <= fsize; i++) {
+
+            memory[MEM_START + i] = buffer[i];
+        }
+        printf("SUCCESS: ROM successfully loaded into memory\n");
+    }
+    else {
+        printf("ERROR: ROM size too large for memory\n");
+    }
 }
 
 uint8_t read_memory(uint16_t address) {
 
+    uint8_t data;
+
+    if(address > 0 && address < MEM_SIZE) {
+
+        data = memory[address];
+        printf("SUCCESS: Data: %i succesfully read from address: %i\n", data, address);
+    }
+    else{
+        //data = NULL;
+        printf("ERROR: Invalid memory address\n");
+    }
+    return data;
+
 }
 
 void write_memory(uint16_t address, uint8_t value) {
+
+    if(address > 0 && address < MEM_SIZE) {
+
+        memory[address] = value;
+        printf("SUCCESS: Value: %i successfully written to address: %i\n", value, address);
+    }
+    else{
+        printf("ERROR: Invalid memory address\n");
+    }
 
 }
